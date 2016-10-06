1e36df3bd5430100d33c8479ba79c89d2c2743c4
Michal Ptaszek <michal.a.ptaszek@gmail.com>
2016-10-07 01:40:55 +0200

    Add Angular 2

---
+++ WebApp/app/app.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h3>Angular 2 Home Component</h3><p>It\'s gonna grow...</p>'
+})
+export class AppComponent { }
